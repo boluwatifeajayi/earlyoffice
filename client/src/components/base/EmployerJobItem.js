fefba187843115946e0dc530b2cf287560f5bd40
boluwatifeajayi <bolu4good@gmail.com>
2023-08-23 23:24:19 +0100

    running

--- client/src/components/base/EmployerJobItem.js
+++ client/src/components/base/EmployerJobItem.js
@@ -11,12 +11,6 @@ function EmployerJobItem({job}) {
 		<div className="my-custom-card-job">
         
 		<div className="spacer">
-		  <div className="intersnhip-box">
-			  <b>{job.jobType}</b>
-		  </div>
-		  <div className="profile-box">
-			  <b className="primary">{job.jobProfile}</b>
-		  </div>
 		</div>
 		<h4>
 			{job.jobName}
@@ -24,14 +18,7 @@ function EmployerJobItem({job}) {
 
 	   
 	
-		<div className="spacer mt-3 mb-4">
-		  <div className="intersnhip-box">
-			  <b>{timeDiff}</b>
-		  </div>
-		  <div className="profile-box">
-			  <b>Openings: <span className='primary'>{job.numberOfOpenings}</span> </b>
-		  </div>
-		</div>
+		
 
 		<div>
 
@@ -39,7 +26,7 @@ function EmployerJobItem({job}) {
 			
 		</div>
 		<Link to={`/employer/internship/${job._id}`}>
-			<button className="bottom-0 end-0 mb-4 me-4 learn-button">Learn More</button>
+			<button className="learn-button">Learn More</button>
 		</Link>
 
 		{/*  <Route path='/employer/internship/:id' element={<EmployerJob/>}/> */}
